7c8e623c2ae190a3714556203d96435bba8df74a
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-14 14:29:54 +0900

    adding springboot files to source

---
+++ src/main/java/household_expenses_managing_api/com/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package household_expenses_managing_api.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("hello world");
+    }
+
+}
