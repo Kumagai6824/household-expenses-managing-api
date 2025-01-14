dac41efffd999556e9dfb2a01b4cd04c8fa658d2
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-14 15:18:04 +0900

    creating income and expense tables in .sql

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,26 +1,29 @@
-DROP TABLE IF EXISTS inventoryProducts;
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS income;
+DROP TABLE IF EXISTS expense;
 
-CREATE TABLE products (
-  id int unsigned AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  deleted_at DATETIME,
+
+CREATE TABLE income (
+  id int AUTO_INCREMENT,
+  type enum('projected', 'actual'),
+  category VARCHAR(50) NOT NULL,
+  amount DECIMAL(10,2),
+  used_date DATE,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
   PRIMARY KEY(id)
 );
 
-INSERT INTO products (id, name, deleted_at) VALUES (1, "Bolt 1", null);
-INSERT INTO products (id, name, deleted_at) VALUES (2, "Washer", null);
+INSERT INTO income (id, type, category, amount , used_date, created_at, updated_at ) VALUES (1, "actual", "salary", 5000, '2024-1-10', null, null);
 
-CREATE TABLE inventoryProducts (
-  id int unsigned AUTO_INCREMENT,
-  product_id int unsigned,
-  quantity int DEFAULT 0,
-  history DATETIME NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY fk_product_id (product_id) REFERENCES products (id)
-  ON DELETE CASCADE
+CREATE TABLE expense (
+  id int AUTO_INCREMENT,
+  type enum('projected', 'actual'),
+  category VARCHAR(50) NOT NULL,
+  amount DECIMAL(10,2),
+  used_date DATE,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY(id)
 );
 
-INSERT INTO inventoryProducts (product_id, quantity, history) VALUES (1, 100, '2023-12-10 23:58:10');
-INSERT INTO inventoryProducts (product_id, quantity, history) VALUES (2, 200, '2024-1-10 12:58:10');
-
+INSERT INTO expense (id, type, category, amount , used_date, created_at, updated_at ) VALUES (1, "actual", "rent", 3000, '2024-1-10', null, null);
