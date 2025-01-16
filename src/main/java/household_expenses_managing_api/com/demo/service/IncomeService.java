9e9c4e7a0dba89ddfedf08bf695ce25c59baa8f6
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-16 23:09:35 +0900

    implemented getAllIncome on service class

---
+++ src/main/java/household_expenses_managing_api/com/demo/service/IncomeService.java
@@ -0,0 +1,9 @@
+package household_expenses_managing_api.com.demo.service;
+
+import household_expenses_managing_api.com.demo.entity.Income;
+
+import java.util.List;
+
+public interface IncomeService {
+    List<Income> getAllIncome();
+}
