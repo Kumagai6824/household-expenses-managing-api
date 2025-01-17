5b40f8eb715f3135a514cb6b49d1069c720da271
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-17 23:59:59 +0900

    added addIncome method to service class

--- src/main/java/household_expenses_managing_api/com/demo/service/IncomeServiceImpl.java
+++ src/main/java/household_expenses_managing_api/com/demo/service/IncomeServiceImpl.java
@@ -4,6 +4,9 @@ import household_expenses_managing_api.com.demo.entity.Income;
 import household_expenses_managing_api.com.demo.mapper.IncomeMapper;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,5 +20,14 @@ public class IncomeServiceImpl implements IncomeService {
     @Override
     public List<Income> getAllIncome() {
         return incomeMapper.getAllIncome();
+    }
+
+    @Override
+    public void addIncome(Income income){
+        Timestamp now=Timestamp.from(Instant.now());
+        income.setCreatedAt(now);
+        income.setUpdatedAt(now);
+
+        incomeMapper.addIncome(income);
     }
 }
