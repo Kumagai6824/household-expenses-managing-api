98929f2cff81e07e35286bc3fb87d416f8f91753
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-19 20:24:20 +0900

    added form test classes/ test type: normal situation

--- src/test/java/household_expenses_managing_api/com/demo/form/ValidationApplicationTests.java
+++ src/test/java/household_expenses_managing_api/com/demo/form/ValidationApplicationTests.java
@@ -1,4 +1,10 @@
 package household_expenses_managing_api.com.demo.form;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ValidationApplicationTests {
+    @Test
+    void contextLoads(){}
 }
