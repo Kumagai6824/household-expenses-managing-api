b6d6e2d56190d293ff70df9dfe2c16e8234a2e5a
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-26 10:08:16 +0900

    added update-income at mapper class

--- src/main/java/household_expenses_managing_api/com/demo/mapper/IncomeMapper.java
+++ src/main/java/household_expenses_managing_api/com/demo/mapper/IncomeMapper.java
@@ -1,12 +1,9 @@
 package household_expenses_managing_api.com.demo.mapper;
 
 import household_expenses_managing_api.com.demo.entity.Income;
-import oracle.jdbc.proxy.annotation.Post;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
@@ -15,6 +12,9 @@ public interface IncomeMapper {
     List<Income> getAllIncome();
 
     @Insert("INSERT INTO income (type, category, amount , used_date, created_at, updated_at ) VALUES (#{type}, #{category}, #{amount} , #{usedDate}, #{createdAt}, #{updatedAt})")
-    @Options(useGeneratedKeys = true,keyProperty = "id")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     void addIncome(Income income);
+
+    @Update("UPDATE income SET type = #{type}, category = #{category}, amount = #{amount}, used_date = #{used_date} WHERE id = #{id}")
+    void updateIncome(int id, Income.Type type, String category, int amount, LocalDate used_date);
 }
