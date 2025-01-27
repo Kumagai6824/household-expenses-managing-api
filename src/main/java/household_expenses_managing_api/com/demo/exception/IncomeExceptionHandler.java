c11f531699af1f49e44fe97dd2a96395d602981d
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-27 10:06:54 +0900

    added ExceptionHandler class

--- src/main/java/household_expenses_managing_api/com/demo/exception/IncomeExceptionHandler.java
+++ src/main/java/household_expenses_managing_api/com/demo/exception/IncomeExceptionHandler.java
@@ -1,10 +1,27 @@
 package household_expenses_managing_api.com.demo.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
 
 @RestControllerAdvice
 public class IncomeExceptionHandler {
 
-    @ExceptionHandler(value = )
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerNoResourceFound(
+            ResourceNotFoundException e, HttpServletRequest request
+    ) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "paht", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+    }
 }
