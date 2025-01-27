72ef2d727beeaf8e7c9ecccf83f2879f800cfceb
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-27 10:06:32 +0900

    added ResourceNotFoundException class

---
+++ src/main/java/household_expenses_managing_api/com/demo/exception/IncomeExceptionHandler.java
@@ -0,0 +1,10 @@
+package household_expenses_managing_api.com.demo.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class IncomeExceptionHandler {
+
+    @ExceptionHandler(value = )
+}
