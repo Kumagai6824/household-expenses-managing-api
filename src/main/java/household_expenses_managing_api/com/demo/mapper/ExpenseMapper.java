37f401c16166707ece284456fa4effd6b64d5abb
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-01-29 20:39:41 +0900

    added expense mapper class/ all methods

--- src/main/java/household_expenses_managing_api/com/demo/mapper/ExpenseMapper.java
+++ src/main/java/household_expenses_managing_api/com/demo/mapper/ExpenseMapper.java
@@ -1,26 +1,26 @@
 package household_expenses_managing_api.com.demo.mapper;
 
-import household_expenses_managing_api.com.demo.entity.Income;
+import household_expenses_managing_api.com.demo.entity.Expense;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @Mapper
-public interface IncomeMapper {
-    @Select("SELECT * FROM income")
-    List<Income> getAllIncome();
+public interface ExpenseMapper {
+    @Select("SELECT * FROM expense")
+    List<Expense> getAllExpense();
 
-    @Insert("INSERT INTO income (type, category, amount , used_date, created_at, updated_at ) VALUES (#{type}, #{category}, #{amount} , #{usedDate}, #{createdAt}, #{updatedAt})")
+    @Insert("INSERT INTO expense (type, category, amount , used_date, created_at, updated_at ) VALUES (#{type}, #{category}, #{amount} , #{usedDate}, #{createdAt}, #{updatedAt})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void addIncome(Income income);
+    void addExpense(Expense expense);
 
-    @Update("UPDATE income SET type = #{type}, category = #{category}, amount = #{amount}, used_date = #{usedDate}, updated_at = #{updatedAt} WHERE id = #{id}")
-    void updateIncome(Income income);
+    @Update("UPDATE expense SET type = #{type}, category = #{category}, amount = #{amount}, used_date = #{usedDate}, updated_at = #{updatedAt} WHERE id = #{id}")
+    void updateExpense(Expense expense);
 
-    @Select("SELECT * FROM income WHERE id = #{id}")
-    Optional<Income> getIncomeById(int id);
+    @Select("SELECT * FROM expense WHERE id = #{id}")
+    Optional<Expense> getExpenseById(int id);
 
-    @Delete("DELETE FROM income WHERE id = #{id}")
-    void deleteIncome(int id);
+    @Delete("DELETE FROM expense WHERE id = #{id}")
+    void deleteExpense(int id);
 }
