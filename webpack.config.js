0818c439ce7b995de86ccc6f65beae3bf638bd3b
Kumagai-java2211 <j.min.howsand682@gmail.com>
2025-08-04 22:54:43 +0900

    Add style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
   },
   mode: "development",
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
